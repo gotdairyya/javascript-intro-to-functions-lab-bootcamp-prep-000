e8cd36520b8fc996f36439a00231c670d06b1099
Derya Akbaba <deryakbaba22@gmail.com>
2017-10-07 23:31:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  var string = string.toUpperCase()
-  console.log('${string}')
+  var shout = string.toUpperCase()
+  console.log('${shout}')
 }
